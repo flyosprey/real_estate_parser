4249b901a9044dce2b09ed68a096338b9d04daab
VladyslavBashtannyk <vbas@jooble.com>
2023-07-20 16:52:29 +0300

    Deleted unneeded

--- perser/spiders/config.py
+++
@@ -1,9 +0,0 @@
-import json
-from pathlib import Path
-
-
-__root = Path(__file__).parent
-
-with open(__root / "permit_numbers.json", 'r') as file:
-    PermitNumbers = json.loads(file.read())["permit_numbers"]
-
